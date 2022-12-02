13187f42102a51d93a509dcfbb85b3a3717a7841
MrUnclePedro <45613023+MrUnclePedro@users.noreply.github.com>
2022-12-01 18:34:51 -0800

    first upload

---
+++ PixLab/pixLab/pixLab/classes/MLRunner.java
@@ -0,0 +1,16 @@
+
+/**
+ * Runs the machine learning image detection program
+ *
+ * @author Peter Olson mrpeterfolson@gmail.com
+ */
+public class MLRunner
+{
+    public static void main( String[] args )
+    {
+        System.out.print('\u000C'); //Clear terminal
+        
+        MLDetector mld = new MLDetector();
+        mld.runDetector( 20/*iterations*/, true/*usingTrainingData -- change this to false if using test data*/ );
+    }
+}
